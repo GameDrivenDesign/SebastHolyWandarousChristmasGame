ebb8d1650d7bcc0055a269dbf943a3ea65dfe1af
Sebastian Schulz <sebastian.schulz95@gmail.com>
2018-11-30 19:14:15 +0100

    add dumb_kid, spaces to tabs for player

---
+++ scenes/dumb_kid/dumb_kid.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+export (int) var max_speed = 200
+export (int) var min_distance = 600
+
+var velocity = Vector2()
+
+var player
+
+func _ready():
+	player = get_parent().get_node("player")
+
+func _process(delta):
+	velocity = Vector2(0.0, 0.0)
+	
+	var target_position = player.position
+	var direction = (target_position - position)
+	
+	var speed = ((min_distance - direction.length()) / min_distance) * max_speed
+	
+	velocity = float(direction.length() <= min_distance) * direction.normalized() * speed
+	
+	rotation = direction.angle()
+
+func _physics_process(delta):
+	move_and_collide(delta*velocity)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,23 +7,21 @@ var velocity = Vector2()
 var rotation_dir = 0
 
 func get_input():
-    rotation_dir = 0
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        rotation_dir += 1
-    if Input.is_action_pressed('left'):
-        rotation_dir -= 1
-    if Input.is_action_pressed('down'):
-        velocity = Vector2(0, speed).rotated(rotation)
-    if Input.is_action_pressed('up'):
-        velocity = Vector2(0, -speed).rotated(rotation)
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('down'):
+		velocity = Vector2(0, speed).rotated(rotation)
+	if Input.is_action_pressed('up'):
+		velocity = Vector2(0, -speed).rotated(rotation)
 
 func _physics_process(delta):
-    get_input()
-    #rotation += rotation_dir * rotation_speed * delta
-    #move_and_slide(velocity)
-    #var impulse_point = Vector2(0.0,$collision_shape.shape.extents.y/3)
-    apply_impulse(Vector2($collision_shape.shape.extents.x / 2, 0.0), delta*Vector2(0.0, rotation_dir*rotation_speed))
-    apply_impulse(Vector2(-$collision_shape.shape.extents.x / 2, 0.0), delta*Vector2(0.0, -rotation_dir*rotation_speed))
-	
-    apply_impulse(Vector2(0.0, 0.0), delta*velocity)
+	get_input()
+
+	apply_impulse(Vector2($collision_shape.shape.extents.x / 2, 0.0), delta*Vector2(0.0, rotation_dir*rotation_speed))
+	apply_impulse(Vector2(-$collision_shape.shape.extents.x / 2, 0.0), delta*Vector2(0.0, -rotation_dir*rotation_speed))
+
+	apply_impulse(Vector2(0.0, 0.0), delta*velocity)
