ab093a706ea60f14a79ead2e5c84ee37eb1c1dbc
Christian Flach <cmfcmf.flach@gmail.com>
2018-11-30 18:20:49 +0100

    Emit signal when time is up

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -1,12 +1,18 @@
 extends Node
 
-var time = 120
+var time = 12
 var gifts = 0
 
+signal time_up
+
 func _ready():
-	pass
+	$elements/gift_counter.text = 'Gifts collected: ' + str(gifts)
 
 func _process(delta):
 	time -= delta
+	if time <= 0:
+		emit_signal("time_up")
+		set_process(false)
+		time = 0
+
 	$elements/timer.text = 'Time remaining: ' + str(round(time))
-	$elements/gift_counter.text = 'Gifts collected: ' + str(gifts)
