72aee3bd0e04ac4880148d5f36bea905b5502aa5
Christian Flach <cmfcmf.flach@gmail.com>
2018-12-01 10:52:36 +0100

    Connect kid is_done signals to hud

---
+++ scenes/world/world.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+onready var name_generator = preload("res://scenes/hud/names.gd").new()
+onready var hud = $ParallaxBackground/ParallaxLayer/hud
+
+func _ready():
+	var kids = get_tree().get_nodes_in_group("dumb_kids")
+	
+	var names = name_generator.get_random_names(kids.size())
+	
+	for i in range(kids.size()):
+		var name = names[i]
+		var kid = kids[i]
+		kid.set_name(name)
+		kid.set_is_good_kid(bool(randi() % 2))
+		kid.connect("is_done", hud, "kid_done")
